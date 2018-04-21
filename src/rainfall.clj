faae4d07a03ec923e7ba0412eda80554e457c876
Shadizzle <mail@shayden.me>
2018-04-21 17:16:32 +1000

    Removed returns function, fixed bug acause  nction, fixed bug caused by confusing doseq with doall 😖

--- src/rainfall.clj
+++ src/rainfall.clj
@@ -1,15 +1,13 @@
 (ns rainfall
   (:require [clojure.edn :as edn]))
 
-(defn returns [val & _] val)
-
 (defn average [coll]
   (/ (reduce + coll) (count coll)))
 
 (defn normalize-rainfall [rainfall]
   (->> rainfall
        (take-while (partial not= -999))
-       (filter (comp not neg?))))
+       (filter (complement neg?))))
 
 (defn rainfall-average [rainfall]
   (let [normalized (normalize-rainfall rainfall)]
@@ -18,10 +16,8 @@
       :no-result)))
 
 (defn -main [& args]
-  (returns nil
-    (let [rainfall-lists    (map edn/read-string args)
-          rainfall-averages (map rainfall-average rainfall-lists)]
-      (doseq (map #(println (str %1 " -> " %2))
-                  rainfall-lists
-                  rainfall-averages)))))
+  (let [rainfall-lists    (map edn/read-string args)
+        rainfall-averages (map rainfall-average rainfall-lists)]
+    (doseq [[list average] (map list rainfall-lists rainfall-averages)]
+      (println (str list " -> " average)))))
 
